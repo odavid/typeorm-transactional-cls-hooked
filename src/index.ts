f72308ec30ba6c9a5f03f2dc55c2e1cf60adac17
Ohad David <Ohad.David@gmail.com>
2019-03-24 23:30:04 +0200

    Added patchTypeORMRepositoryWithBaseRepository (#14)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 export { BaseRepository } from './BaseRepository'
 export { initializeTransactionalContext, getEntityManagerOrTransactionManager } from './common'
 export { runOnTransactionCommit, runOnTransactionComplete, runOnTransactionRollback } from './hook'
+export { patchTypeORMRepositoryWithBaseRepository } from './patch-typeorm-repository'
 export { Propagation } from './Propagation'
 export { IsolationLevel } from './IsolationLevel'
 export { Transactional } from './Transactional'
