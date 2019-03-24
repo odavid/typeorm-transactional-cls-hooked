331344e56c804d1e57ff20d504025b426c5e8f12
Ohad David <Ohad.David@gmail.com>
2019-03-24 22:55:38 +0200

    externalize getEntityManagerOrTransactionManager from BaseRepository and exported it for using it outside the BaseRepository (#13)

--- src/common.ts
+++ src/common.ts
@@ -1,6 +1,6 @@
 import { createNamespace, getNamespace, Namespace } from 'cls-hooked'
 import { EventEmitter } from 'events'
-import { EntityManager } from 'typeorm'
+import { EntityManager, getManager } from 'typeorm'
 
 export const NAMESPACE_NAME = '__typeOrm___cls_hooked_tx_namespace'
 
@@ -9,6 +9,22 @@ const TYPE_ORM_HOOK_KEY = '__typeOrm__transactionalCommitHooks'
 
 export const initializeTransactionalContext = () =>
   getNamespace(NAMESPACE_NAME) || createNamespace(NAMESPACE_NAME)
+
+export const getEntityManagerOrTransactionManager = (
+  connectionName: string,
+  entityManager: EntityManager | undefined
+): EntityManager => {
+  const context = getNamespace(NAMESPACE_NAME)
+
+  if (context && context.active) {
+    const transactionalEntityManager = getEntityManagerForConnection(connectionName, context)
+
+    if (transactionalEntityManager) {
+      return transactionalEntityManager
+    }
+  }
+  return entityManager || getManager(connectionName)
+}
 
 export const getEntityManagerForConnection = (
   connectionName: string,
@@ -23,15 +39,10 @@ export const setEntityManagerForConnection = (
   entityManager: EntityManager | null
 ) => context.set(`${TYPE_ORM_KEY_PREFIX}${connectionName}`, entityManager)
 
-export const getHookInContext = (
-  context: Namespace
-): EventEmitter | null => {
+export const getHookInContext = (context: Namespace): EventEmitter | null => {
   return context.get(TYPE_ORM_HOOK_KEY)
 }
 
-export const setHookInContext = (
-  context: Namespace,
-  emitter: EventEmitter | null
-) => {
+export const setHookInContext = (context: Namespace, emitter: EventEmitter | null) => {
   return context.set(TYPE_ORM_HOOK_KEY, emitter)
 }
