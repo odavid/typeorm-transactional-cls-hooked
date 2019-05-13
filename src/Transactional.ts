a6c053fa960c978dac91b069f4450e870776e8b5
Ashley Williams <hi@ashleyw.co.uk>
2019-05-13 11:36:58 +0000

    feat: allow use of scoped containers

--- src/Transactional.ts
+++ src/Transactional.ts
@@ -1,8 +1,11 @@
 import { getNamespace } from 'cls-hooked'
-import { EntityManager, getManager } from 'typeorm'
+import Container from 'typedi'
+import { ContainerInstance } from 'typedi'
+import { ConnectionManager, EntityManager, getManager } from 'typeorm'
+
 import {
-  getEntityManagerForConnection,
   NAMESPACE_NAME,
+  getEntityManagerForConnection,
   setEntityManagerForConnection,
 } from './common'
 import { runInNewHookContext } from './hook'
@@ -21,17 +24,17 @@ export function Transactional(options?: {
   propagation?: Propagation
   isolationLevel?: IsolationLevel
 }): MethodDecorator {
-  const connectionName: string =
-    options && options.connectionName ? options.connectionName : 'default'
-  const propagation: Propagation =
-    options && options.propagation ? options.propagation : Propagation.REQUIRED
+  const connectionName: string = options && options.connectionName ? options.connectionName : 'default' // prettier-ignore
+  const propagation: Propagation = options && options.propagation ? options.propagation : Propagation.REQUIRED // prettier-ignore
   const isolationLevel: IsolationLevel | undefined = options && options.isolationLevel
 
   return (target: any, methodName: string | symbol, descriptor: TypedPropertyDescriptor<any>) => {
     const originalMethod = descriptor.value
 
-    descriptor.value = function(...args: any[]) {
+    descriptor.value = function(this: { container?: ContainerInstance }, ...args: any[]) {
       const context = getNamespace(NAMESPACE_NAME)
+      const container = this.container || Container.of(undefined)
+      
       if (!context) {
         throw new Error(
           'No CLS namespace defined in your app ... please call initializeTransactionalContext() before application start.'
@@ -49,13 +52,19 @@ export function Transactional(options?: {
           return result
         }
 
+        const connectionManager = container.get(ConnectionManager)
+
+        const connection = connectionManager
+          ? connectionManager.get(connectionName)
+          : getManager(connectionName)
+
         if (isolationLevel) {
           return await runInNewHookContext(context, () =>
-            getManager(connectionName).transaction(isolationLevel, transactionCallback)
+            connection.transaction(isolationLevel, transactionCallback)
           )
         } else {
           return await runInNewHookContext(context, () =>
-            getManager(connectionName).transaction(transactionCallback)
+            connection.transaction(transactionCallback)
           )
         }
       }
