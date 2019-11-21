56d0dd6aecfbc7525366737040f7eb2c44f8ad74
Simon Davey <simonr.davey@gmail.com>
2019-11-21 09:35:30 +0000

    feat: adds patchTypeORMTreeRepositoryWithBaseTreeRepository method

--- src/patch-typeorm-repository.ts
+++ src/patch-typeorm-repository.ts
@@ -1,10 +1,20 @@
-import { Repository } from 'typeorm'
+import { Repository, TreeRepository } from 'typeorm'
 import { BaseRepository } from './BaseRepository'
+import { BaseTreeRepository } from './BaseTreeRepository'
 
 export const patchTypeORMRepositoryWithBaseRepository = () => {
   Object.getOwnPropertyNames(BaseRepository.prototype).forEach(pName =>
     Object.defineProperty(Repository.prototype, pName, Object.getOwnPropertyDescriptor(
       BaseRepository.prototype,
+      pName
+    ) as PropertyDescriptor)
+  )
+}
+
+export const patchTypeORMTreeRepositoryWithBaseTreeRepository = () => {
+  Object.getOwnPropertyNames(BaseTreeRepository.prototype).forEach(pName =>
+    Object.defineProperty(TreeRepository.prototype, pName, Object.getOwnPropertyDescriptor(
+      BaseTreeRepository.prototype,
       pName
     ) as PropertyDescriptor)
   )
