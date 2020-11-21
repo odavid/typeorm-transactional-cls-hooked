563e42fa50589db66bd9f253f300edc105d548b0
odavid <ohad.david@gmail.com>
2020-11-21 17:58:45 +0200

    initial test structure

---
+++ tests/entity/Post.ts
@@ -0,0 +1,11 @@
+import {Entity, PrimaryGeneratedColumn, Column} from "typeorm";
+
+@Entity()
+export class Post {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  message: string;
+
+}
