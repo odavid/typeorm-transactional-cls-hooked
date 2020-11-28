1a65ed52d135e916870ccbae30f31ff74216c9eb
odavid <ohad.david@gmail.com>
2020-11-28 16:03:17 +0200

    NestJS test + test refactoring

---
+++ tests/__tests__/test_nestjs.ts
@@ -0,0 +1,64 @@
+import { NestFactory } from "@nestjs/core"
+import { getConnection, Repository } from "typeorm"
+import { initializeTransactionalContext, patchTypeORMRepositoryWithBaseRepository } from "../../src"
+import { Post } from "../entity/Post"
+import {AppService } from "../nestjs/app.service"
+import { Test, TestingModule } from "@nestjs/testing"
+import { TypeOrmModule } from "@nestjs/typeorm"
+
+describe("NestJS", () => {
+  let app: TestingModule
+  let service: AppService
+  beforeAll(async () => {
+    initializeTransactionalContext()
+    patchTypeORMRepositoryWithBaseRepository()
+    app = await Test.createTestingModule({
+      imports: [
+        TypeOrmModule.forRoot({
+          type: 'postgres',
+          host: 'localhost',
+          port: 5432,
+          username: 'postgres',
+          password: 'postgres',
+          entities: [Post],
+          synchronize: true,
+          logging: "all",
+        }),
+          TypeOrmModule.forFeature([Post])
+      ],
+      exports: [],
+      providers: [AppService],
+    }).compile()
+    service = app.get<AppService>(AppService)
+  })
+
+  afterAll(async () => await app.close())
+
+  it('Creates a post using service', async (done) => {
+    const message = "NestJS - A successful post"
+    const post = await service.createPost(message)
+    expect(post.id).toBeGreaterThan(0)
+    setTimeout(async () => {
+      expect(service.success).toEqual("true")
+      const dbPost = await service.getPostByMessage(message)
+      console.log(`dbPost: ${dbPost}`)
+      expect(dbPost).toBeTruthy()
+      done()
+    }, 1000)
+  })
+
+  it('Fails creating a post using service', async (done) => {
+    const message = "NestJS - An unsuccessful post"
+    try{
+      const post = await service.createPost(message, true)
+    }catch(e){
+      setTimeout(async () => {
+        expect(service.success).toEqual("false")
+        const dbPost = await service.getPostByMessage(message)
+        console.log(`dbPost: ${dbPost}`)
+        expect(dbPost).toBeFalsy()
+        done()
+      }, 1000)
+    }
+  })
+})
---
+++ tests/__tests__/test_simple.ts
@@ -0,0 +1,58 @@
+import 'reflect-metadata'
+
+import {initializeTransactionalContext, patchTypeORMRepositoryWithBaseRepository, Transactional, runOnTransactionCommit, runOnTransactionRollback} from '../../src'
+import { createConnection, getConnection, Repository } from 'typeorm';
+import {Post} from '../entity/Post'
+import {SimpleService} from '../simple/simple.service'
+
+
+describe("Simple", () => {
+  beforeAll(async () => {
+    initializeTransactionalContext()
+    patchTypeORMRepositoryWithBaseRepository()
+    const conn = await createConnection({
+      type: 'postgres',
+      host: 'localhost',
+      port: 5432,
+      username: 'postgres',
+      password: 'postgres',
+      entities: [Post],
+      synchronize: true,
+      logging: "all",
+    })
+  })
+
+  afterAll(async () => await getConnection().close())
+
+  it('Creates a post using service', async (done) => {
+    const repository = await getConnection().getRepository(Post)
+    const service = new SimpleService(repository)
+    const message = "simple - A successful post"
+    const post = await service.createPost(message)
+    expect(post.id).toBeGreaterThan(0)
+    setTimeout(async () => {
+      expect(service.success).toEqual("true")
+      const dbPost = await service.getPostByMessage(message)
+      console.log(`dbPost: ${dbPost}`)
+      expect(dbPost).toBeTruthy()
+      done()
+    }, 1000)
+  })
+
+  it('Fails creating a post using service', async (done) => {
+    const repository = await getConnection().getRepository(Post)
+    const service = new SimpleService(repository)
+    const message = "simple - An unsuccessful post"
+    try{
+      const post = await service.createPost(message, true)
+    }catch(e){
+      setTimeout(async () => {
+        expect(service.success).toEqual("false")
+        const dbPost = await service.getPostByMessage(message)
+        console.log(`dbPost: ${dbPost}`)
+        expect(dbPost).toBeFalsy()
+        done()
+      }, 1000)
+    }
+  })
+})
