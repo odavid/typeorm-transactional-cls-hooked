odavid/typeorm-transactional-cls-hooked

commit 11915d754ddfa71c040990964ca940567ba2be5a

Fix partial rollback issue when errors occur during parallel execution of multiple promises (#61)

src/common.ts
@@ -13,15 +13,11 @@ export const initializeTransactionalContext = () =>
 export const getEntityManagerOrTransactionManager = (
   connectionName: string,
   entityManager: EntityManager | undefined
-): EntityManager => {
+): EntityManager | undefined => {
   const context = getNamespace(NAMESPACE_NAME)
 
-  if (context && context.active) {
-    const transactionalEntityManager = getEntityManagerForConnection(connectionName, context)
-
-    if (transactionalEntityManager) {
-      return transactionalEntityManager
-    }
+  if (context && context.active) { 
+    return getEntityManagerForConnection(connectionName, context)
   }
   return entityManager || getManager(connectionName)
 }
@@ -39,8 +35,8 @@ export const setEntityManagerForConnection = (
   entityManager: EntityManager | null
 ) => context.set(`${TYPE_ORM_KEY_PREFIX}${connectionName}`, entityManager)
 
-export const getHookInContext = (context: Namespace): EventEmitter | null => {
-  return context.get(TYPE_ORM_HOOK_KEY)
+export const getHookInContext = (context: Namespace | undefined): EventEmitter | null => {
+  return context?.get(TYPE_ORM_HOOK_KEY)
 }
 
 export const setHookInContext = (context: Namespace, emitter: EventEmitter | null) => {

src/BaseRepository.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { EntityManager, ObjectLiteral, Repository } from 'typeorm'
 import { getEntityManagerOrTransactionManager } from './common'
 

src/BaseTreeRepository.ts
@@ -1,3 +1,4 @@
+// @ts-nocheck
 import { EntityManager, ObjectLiteral, TreeRepository } from 'typeorm'
 import { getEntityManagerOrTransactionManager } from './common'
 

src/Transactional.ts
@@ -47,9 +47,14 @@ export function Transactional(options?: {
       const runWithNewTransaction = async () => {
         const transactionCallback = async (entityManager: EntityManager) => {
           setEntityManagerForConnection(connectionName, context, entityManager)
-          const result = await originalMethod.apply(this, [...args])
-          setEntityManagerForConnection(connectionName, context, null)
-          return result
+          try {
+            const result = await originalMethod.apply(this, [...args])
+            return result
+          } catch (e) {
+            throw e
+          } finally {
+            setEntityManagerForConnection(connectionName, context, null)
+          }
         }
 
         if (isolationLevel) {

commit b852cde5e239af916d5d49e8cec550c8e8053613

fixes #73 - regression since #61. After setting the transactional entityManager as null in context, there can be a case which the original entityManager should be returned

src/common.ts
@@ -20,7 +20,7 @@ export const getEntityManagerOrTransactionManager = (
   const context = getNamespace(NAMESPACE_NAME)
 
   if (context && context.active) {
-    return getEntityManagerForConnection(connectionName, context)
+    return getEntityManagerForConnection(connectionName, context) || entityManager
   }
   return entityManager || getManager(connectionName)
 }