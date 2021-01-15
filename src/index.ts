825aa08d7c663f3bdbf83e10cf94b1a60a43d274
Ohad David <Ohad.David@gmail.com>
2021-01-15 15:23:56 +0200

    Fixes #82 - Exporting NAMESPACE_NAME (#84)

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { BaseRepository } from './BaseRepository'
 export { BaseTreeRepository } from './BaseTreeRepository'
-export { initializeTransactionalContext, getEntityManagerOrTransactionManager } from './common'
+export { initializeTransactionalContext, getEntityManagerOrTransactionManager, NAMESPACE_NAME } from './common'
 export { runOnTransactionCommit, runOnTransactionComplete, runOnTransactionRollback } from './hook'
 export {
   patchTypeORMRepositoryWithBaseRepository,
